c37f97d9ce44d5385b9e834fa8610624d434e2d8
carlos <carlosthe19916@gmail.com>
2017-04-03 23:58:45 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { ConfigService, configServiceInitializer } from './config.service';
 
-import { AppRoutingModule } from './app-routing.module';
+import { APP_INITIALIZER } from '@angular/core';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BsDropdownModule } from "ng2-bootstrap";
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+import { HttpModule } from '@angular/http';
+import { LocalStorageModule } from 'angular-2-local-storage';
+import { NgModule } from '@angular/core';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { RestangularModule } from "ng2-restangular";
+
+export function restangularProviderConfigurer(restangularProvider: any, config: ConfigService) {
+  restangularProvider.setBaseUrl(config.getSettings().apiEndpoint);
+}
 
 @NgModule({
   declarations: [
@@ -16,9 +26,24 @@ import { HeaderComponent } from './header/header.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    RestangularModule.forRoot([ConfigService], restangularProviderConfigurer),
+    NgbModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    LocalStorageModule.withConfig({
+      prefix: 'openfact',
+      storageType: 'localStorage'
+    }),
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    ConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configServiceInitializer,
+      deps: [ConfigService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/config.service.ts
@@ -0,0 +1,55 @@
+import * as _ from 'lodash';
+
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+const defaults = Object.freeze({
+  apiEndpoint: 'http://localhost:8080/rest',
+  title: 'Openfact',
+});
+
+const defaultConfigJson = '/config.json';
+
+export function configServiceInitializer(config: ConfigService) {
+  return () => config.load();
+}
+
+@Injectable()
+export class ConfigService {
+
+  private settingsRepository: any = defaults;
+
+  constructor(private _http: Http) { }
+
+  load(configJson: string = defaultConfigJson): Promise<ConfigService> {
+    return this._http.get(configJson).map(res => res.json())
+      .toPromise()
+      .then((config) => {
+        this.settingsRepository = Object.freeze(_.merge({}, this.settingsRepository, config));
+      })
+      .catch(() => {
+        console.log('Error: Configuration service unreachable!');
+      });
+  }
+
+  getSettings(group?: string, key?: string): any {
+    if (!group) {
+      return this.settingsRepository;
+    }
+
+    if (!this.settingsRepository[group]) {
+      throw new Error(`Error: No setting found with the specified group [${group}]!`);
+    }
+
+    if (!key) {
+      return this.settingsRepository[group];
+    }
+
+    if (!this.settingsRepository[group][key]) {
+      throw new Error(`Error: No setting found with the specified group/key [${group}/${key}]!`);
+    }
+
+    return this.settingsRepository[group][key];
+  }
+
+}
