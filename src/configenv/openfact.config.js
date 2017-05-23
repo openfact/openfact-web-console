f3ce8f47577fde2c0078bccec08700fe8972fb34
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-05-23 11:26:37 -0500

    changes

--- src/configenv/openfact.config.js
+++ src/configenv/openfact.config.js
@@ -1,7 +1,7 @@
 var config = {
-  API_ENDPOINT: (process.env.OPENFACT_ENDPOINT != null ? process.env.OPENFACT_ENDPOINT : process.env.OPENFACT_SERVICE + '-' + process.env.OPENSHIFT_BUILD_NAMESPACE),
-  SECURE_API_ENDPOINT: (process.env.SECURE_OPENFACT_ENDPOINT != null ? process.env.SECURE_OPENFACT_ENDPOINT : process.env.SECURE_OPENFACT_SERVICE + '-' + process.env.SECURE_OPENFACT_SERVICE),
-  SSO_ENABLED: process.env.KEYCLOAK_URL ? true : false
+  apiEndpoint: (process.env.OPENFACT_ENDPOINT != null ? process.env.OPENFACT_ENDPOINT : process.env.OPENFACT_SERVICE + '-' + process.env.OPENSHIFT_BUILD_NAMESPACE),
+  secureApiEndpoint: (process.env.SECURE_OPENFACT_ENDPOINT != null ? process.env.SECURE_OPENFACT_ENDPOINT : process.env.SECURE_OPENFACT_SERVICE + '-' + process.env.SECURE_OPENFACT_SERVICE),
+  keycloakEnabled: process.env.KEYCLOAK_URL ? true : false
 };
 
 module.exports = config;
